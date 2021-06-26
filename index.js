3625e48dfbd28493168b855ffdcc45a0431312b3
PushpenderSaini0 <54404738+PushpenderSaini0@users.noreply.github.com>
2021-06-27 01:46:47 +0530

    Base Setup

--- index.js
+++ index.js
@@ -8,3 +8,20 @@ const server = app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
 })
 const io = webSocket.init(server);
+
+
+io.sockets.on('connection', (socket) => {
+    // console.log(socket.adapter.rooms);
+    socket.on('HOST-ROOM', (data, callback) => {
+        const roomKey = Date.now().toString(16);;
+        socket.join(roomKey);
+        callback(roomKey);
+    });
+    socket.on('PUSH-CODE', (obj) => {
+        io.to(obj.ROOMKEY).emit('CODE-SERVE', obj.code);
+    });
+    socket.on('JOIN-ROOM', (roomkey, callback) => {
+        socket.join(roomkey);
+        callback();
+    });
+});
--- public/js/main.js
+++ public/js/main.js
@@ -1,11 +1,34 @@
 const SOCKET = io.connect("ws://localhost:3000/");;
-
+let ROOMKEY = null;
 const btnClickHandler = event => {
-    if (event.target.id === "host-btn")
-        console.log("Host")
-    if (event.target.id === "join-btn")
-        console.log("Join")
+    if (event.target.id === "host-btn") {
+        const hostName = document.getElementById("host-name").value;
+        SOCKET.emit('HOST-ROOM', { hostName }, (roomKey) => {
+            ROOMKEY = roomKey;
+            document.getElementById("init").innerHTML = "";
+            document.getElementById("room-key-msg").innerHTML = "Your Hosted roomkey is : " + roomKey;
+            document.getElementById("code").addEventListener("change", sendCode);
+        });
+    }
+    if (event.target.id === "join-btn") {
+        ROOMKEY = document.getElementById("room-key").value;
+        SOCKET.emit("JOIN-ROOM", ROOMKEY, () => {
+            document.getElementById("init").innerHTML = "";
+            document.getElementById("code").disabled = true;
+            SOCKET.on("CODE-SERVE",codeUpdater);
+        });
+    }
 }
+
+const sendCode = () => {
+    SOCKET.emit('PUSH-CODE', { ROOMKEY, code: document.getElementById("code").value });
+}
+
+
+const codeUpdater = code => {
+    document.getElementById("code").value = code;
+}
+
 
 
 
