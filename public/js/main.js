7863e7b31d9b75bb4b93f823679041cb1870e8cb
PushpenderSaini0 <54404738+PushpenderSaini0@users.noreply.github.com>
2021-06-26 13:02:58 +0530

    Added basic Homepage

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,12 @@
-console.log("Hello-World")
+const btnClickHandler = event => {
+    if (event.target.id === "host-btn")
+        console.log("Host")
+    if (event.target.id === "join-btn")
+        console.log("Join")
+}
+
+
+
+//set click listeners
+document.getElementById("host-btn").addEventListener("click", btnClickHandler);
+document.getElementById("join-btn").addEventListener("click", btnClickHandler);
