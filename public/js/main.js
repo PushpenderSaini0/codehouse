6b435314ad74b0869d3e3d87c25a112ae43e12d7
PushpenderSaini0 <54404738+PushpenderSaini0@users.noreply.github.com>
2021-06-29 10:19:51 +0530

    Added a button to download code as file

--- public/js/main.js
+++ public/js/main.js
@@ -1,23 +1,28 @@
 import { hostRoom, joinRoom } from './wsHandler.js';
-import { initEditor } from './editor.js';
+import { initEditor, getEditorValue } from './editor.js';
+import { downloadAsFile } from './downloadAsFileHelper.js';
 
 const btnClickHandler = event => {
     if (event.target.id === "host-btn") {
         hostRoom();
         initEditor('HOST');
         //force-height-bug-fix
-        const h = document.body.clientHeight - 50 ;
+        const h = document.body.clientHeight - 50;
         document.getElementById("code").setAttribute("style", `height:${h}px`);
     }
     if (event.target.id === "join-btn") {
         joinRoom();
         initEditor('MEMBER');
         //force-height-bug-fix
-        const h = document.body.clientHeight - 50 ;
+        const h = document.body.clientHeight - 50;
         document.getElementById("code").setAttribute("style", `height:${h}px`);
+    }
+    if (event.target.id === "download-file-btn") {
+        downloadAsFile(getEditorValue(), "codehouse.js");
     }
 }
 
 //set click listeners
+document.getElementById("download-file-btn").addEventListener("click", btnClickHandler);
 document.getElementById("host-btn").addEventListener("click", btnClickHandler);
 document.getElementById("join-btn").addEventListener("click", btnClickHandler);
